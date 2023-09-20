f1811888eb6907a1f3b3ab7bbd57ba63d9c246fd
Your Name <you@example.com>
2023-09-20 23:17:26 +0200

    updated to latest version of gamba and fixed redeem button

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import { Button } from '../components/Button';
-import { Section } from '../components/Section';
-import { Icon } from '../components/Icon';
-import styles from './Home.module.css';
-import { useNavigate } from 'react-router-dom';
-
-
-
+import React from "react";
+import { Button } from "../components/Button";
+import { Section } from "../components/Section";
+import { Icon } from "../components/Icon";
+import styles from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
 
 export function Home() {
   const navigate = useNavigate();
 
   const handlePlayClick = () => {
-    navigate('/flip');
+    navigate("/flip");
   };
 
   return (
     <div className={styles.banner}>
       <Section>
-        <img src="logo.png" alt="Logo" style={{ maxWidth: '200px', margin: '0 auto' }} />
-        <p >
-          A on-chain, provably-fair app built on Gamba.
-        </p>
-        <div style={{ display: 'flex', gap: '10px', justifyContent: 'center', margin: '0 auto' }}>
-        <Button
-          as="a"
-          color="white" 
-          onClick={handlePlayClick}
-          icon={<Icon.ExternalLink />}
+        <img
+          src="logo.png"
+          alt="Logo"
+          style={{ maxWidth: "200px", margin: "0 auto" }}
+        />
+        <p>An on-chain, provably-fair app built on Gamba.</p>
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            justifyContent: "center",
+            margin: "40px auto",
+          }}
         >
-          Play
-        </Button>
-
+          <Button
+            as="a"
+            color="white"
+            onClick={handlePlayClick}
+            icon={<Icon.ExternalLink />}
+          >
+            Play
+          </Button>
         </div>
       </Section>
     </div>
